505faff6a3d892acb36b44cc0579257b295aa15b
sochin.jiang <johnpub@zju.edu.cn>
2017-07-12 23:05:19 +0800

    feat: add nbd materials

---
+++ nbd/start-nbdserver.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: start.sh
+# Author: sochin
+# mail: joinpub@163.com
+# Created Time: Sat 03 Dec 2016 12:23:24 AM EST
+#########################################################################
+#!/bin/bash
+nbd-server -C /mnt/images/nbd-disks/config
+
+
